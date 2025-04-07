d534391079b6a1b09c69be2d40e98e2d5ee0aa04
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-07 17:42:30 -0300

    findAll, findById

--- src/main/java/com/course/bloco_de_notas/resources/NotaResource.java
+++ src/main/java/com/course/bloco_de_notas/resources/NotaResource.java
@@ -1,17 +1,31 @@
 package com.course.bloco_de_notas.resources;
 
 import com.course.bloco_de_notas.entities.Nota;
+import com.course.bloco_de_notas.services.NotaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/notas")
 public class NotaResource {
+    @Autowired
+    private NotaService service;
+
     @GetMapping
-    public ResponseEntity<Nota> findAll() {
-        Nota nota1 = new Nota(1L, "Teste", "Testando", 1);
-        return ResponseEntity.ok().body(nota1);
+    public ResponseEntity<List<Nota>> findAll() {
+        List<Nota> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Nota> findById(@PathVariable Long id) {
+        Nota obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/course/bloco_de_notas/services/NotaService.java
@@ -0,0 +1,24 @@
+package com.course.bloco_de_notas.services;
+
+import com.course.bloco_de_notas.entities.Nota;
+import com.course.bloco_de_notas.repositories.NotaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NotaService {
+    @Autowired
+    private NotaRepository repository;
+
+    public List<Nota> findAll() {
+        return repository.findAll();
+    }
+
+    public Nota findById(Long id) {
+        Optional<Nota> obj = repository.findById(id);
+        return obj.get();
+    }
+}
