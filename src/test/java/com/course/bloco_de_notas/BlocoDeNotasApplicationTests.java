8ef6f83fd55cb7adcab0b5996f602548aef3cd18
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-07 16:37:45 -0300

    projeto criado

---
+++ src/test/java/com/course/bloco_de_notas/BlocoDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.bloco_de_notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocoDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
